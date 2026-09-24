a42d67a2aa13325d3cdec3018878d0ff0e3148b1
guntreddiabhiramnaidu-droid <guntreddiabhiramnaidu@klh.edu.in>
2026-09-24 13:43:09 +0530

    Add files via upload

---
+++ src/Week2/Table.java
@@ -0,0 +1,8 @@
+public class Table {
+    public static void main(String[] args) {
+        System.out.printf("%-10s %6s%n", "Locker", "Count");
+        System.out.printf("%-10s %6d%n", "Small", 12);
+        System.out.printf("%-10s %6d%n", "Large", 3);
+        System.out.printf("%-10s %6d%n", "XL", 137);
+    }
+}
